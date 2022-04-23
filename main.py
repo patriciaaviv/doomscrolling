f7ba7c192768d6d9f579182b7299d2c9359c2d55
xilefcul <avivchro@gmail.com>
2022-04-24 00:23:31 +0200

    more data cleaning

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
 import re, string, random
+import cleantext
 
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import twitter_samples, stopwords
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize
 from nltk import FreqDist, classify, NaiveBayesClassifier
+from cleantext import clean
 
 # Read data from yesterday
 tweets_2204 = pd.read_csv('0422_UkraineCombinedTweetsDeduped.csv', low_memory=False)
@@ -22,10 +24,45 @@ df_tweets = tweets_2204.drop(
 # drop non-english tweets
 df_tweets = df_tweets[df_tweets['language'] == "en"]
 
-# TODO for later, check if user is bot, for now skipping
+# TODO: for later, check if user is bot, for now skipping
 
 # drop non-ascii characters, if ascii character is detected -> replace with ""
-tweets_2204['text'] = tweets_2204['text'].replace(to_replace="/[^ -~]+/g", value="", regex=True)
+df_tweets['text'] = df_tweets['text'].replace(to_replace="/[^ -~]+/g", value="", regex=True)
+
+# remove emojis
+filter_char = lambda c: ord(c) < 256
+df_tweets['text'] = df_tweets['text'].apply(lambda s: ''.join(filter(filter_char, s)))
+df_tweets['text'].astype(str).apply(lambda x: x.encode('latin-1', 'ignore').decode('latin-1'))
+
+# drop tweets that are now empty
+df_tweets = df_tweets[df_tweets['text'] != ""]
+
+# remove mentions, underscore from mentions remains?
+df_tweets['text'] = df_tweets['text'].str.replace('@[A-Za-z0-9]+\s?', '', regex=True)
+
+# remove underscore
+df_tweets['text'] = df_tweets['text'].str.replace('_', '', regex=True)
+
+# remove hashtags
+df_tweets['text'] = df_tweets['text'].str.replace('#', '', regex=True)
+
+df_tweets['text'] = df_tweets['text'].str.replace('.', '', regex=True)
+df_tweets['text'] = df_tweets['text'].str.replace(',', '', regex=True)
+
+# remove html code
+# df_tweets['text'] = [re.sub(r'/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-fA-F]{1,6});/ig','', str(x)) for x in df_tweets['text']]
+# df_tweets['text'].replace({r"/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-fA-F]{1,6});/ig": ''}, inplace=True, regex=True)
+# both approaches don't work, fix with hardcode for now
+df_tweets['text'] = df_tweets['text'].str.replace('&amp', '', regex=True)
+df_tweets['text'] = df_tweets['text'].str.replace('&gt', '', regex=True)
+df_tweets['text'] = df_tweets['text'].str.replace('&lt', '', regex=True)
+
+df_tweets.to_csv('clean_data.csv', encoding='utf-8', index=True)
+
+
+# tokenize
+
+# drop numbers?
 
 
 def remove_noise(tweet_tokens, stop_words=()):
