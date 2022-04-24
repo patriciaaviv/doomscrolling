3e92cc653cebf794120f0712c99fd6b4ab965b2d
xilefcul <avivchro@gmail.com>
2022-04-24 05:11:23 +0200

    added training model

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import wordnet
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 # Read data from yesterday
 tweets_2204 = pd.read_csv('0422_UkraineCombinedTweetsDeduped.csv', low_memory=False)
@@ -84,11 +85,118 @@ df_tweets_tokenized = df_tweets['text'].apply(regexp.tokenize)
 
 # make new csv with clean data set for testing
 df_tweets.to_csv('clean_data.csv', encoding='utf-8', index=True)
-df_tweets_tokenized.to_csv('clean_data_tokenize.csv', encoding='utf-8', index=True)
+df_tweets_tokenized.to_csv('clean_data_tokenize.csv', encoding='utf-8', index=False)
 print('finished csv transformation')
 
-# more accuracy -> implement lemmatization
+
+# more accuracy -> implement lemmatization and/or stemming
+
+
+#
+# preprocessing functions
+#
+
+def remove_noise(tweet_tokens, stop_words=()):
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|' \
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
+        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    return cleaned_tokens
+
+
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+
+
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
 
 
 
 
+
+if __name__ == "__main__":
+
+    # training set
+    positive_tweets = twitter_samples.strings('positive_tweets.json')
+    negative_tweets = twitter_samples.strings('negative_tweets.json')
+
+    # text = twitter_samples.strings('tweets.20150430-223406.json')
+
+    tweet_tokens = twitter_samples.tokenized('positive_tweets.json')[0]
+
+    # remove stopwords
+    stop_words = stopwords.words('english')
+
+    # tokenize training data set from nltk library
+    positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+    negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+
+    positive_cleaned_tokens_list = []
+    negative_cleaned_tokens_list = []
+
+    # preprocessing of training data
+    for tokens in positive_tweet_tokens:
+        positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+    for tokens in negative_tweet_tokens:
+        negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+
+    all_pos_words = get_all_words(positive_cleaned_tokens_list)
+
+    freq_dist_pos = FreqDist(all_pos_words)
+    print(freq_dist_pos.most_common(10))
+
+    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
+    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
+
+    positive_dataset = [(tweet_dict, "Positive")
+                        for tweet_dict in positive_tokens_for_model]
+
+    negative_dataset = [(tweet_dict, "Negative")
+                        for tweet_dict in negative_tokens_for_model]
+
+    dataset = positive_dataset + negative_dataset
+
+    random.shuffle(dataset)
+
+    train_data = dataset[:7000]
+    test_data = dataset[7000:]
+
+    classifier = NaiveBayesClassifier.train(train_data)
+
+    print("Accuracy is:", classify.accuracy(classifier, test_data))
+
+    print(classifier.show_most_informative_features(10))
+
+    # apply classifier on our data set
+    #print(df_tweets_tokenized[4], classifier.classify(dict([token, True] for token in df_tweets_tokenized[4])))
+    # iterate through tokenized df and apply classify function to each value in 'text'
+    # pass the results in format "tweet" : "positive/negative' in result df
+
+    # generate new df for saving the results
+    data = {'tweet':[], 'result':[]}
+    df_result = pd.DataFrame(data)
+
+    # iterate through token df and apply function
+    
+
+    # df_results = df_tweets_tokenized['text'].apply(lambda x: classifier.classify(dict))
